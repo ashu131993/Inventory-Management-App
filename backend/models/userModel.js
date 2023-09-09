cf2e2b531f61b616ada5b595ce18f92ce97e2974
ashu131993 <ashu131993@gmail.com>
2023-09-09 22:02:52 +0530

    hashed password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,9 @@
-const mongoose = require("mangoose")
+const mongoose = require("mongoose");
 
-const userSchema = mangoose.Schema({
+const userSchema = mongoose.Schema({
    name: {
     type: String,
-    required: [true, "Please add a name"]
+    required: [true, "Please add a name"],
    },
    email: {
     type: String,
@@ -12,33 +12,35 @@ const userSchema = mangoose.Schema({
     trim: true,
     match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid email"
-    ]
+        "Please enter a valid email",
+    ],
    },
    password: {
     type:String,
     required: [true, "Please enter a valid Password"],
     minLength: [6, "Password must be upto 6 character"],
-    maxLength: [6, "Password must not be longer than 23 character"],
+    // maxLength: [23, "Password must not be longer than 23 character"],
    },
    photo: {
     type:String,
     required: [true, "Please add a photo"],
-    default: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1694173844~exp=1694174444~hmac=9f6dbea3ec57a24bafc0852688622c6a605c4a70e71d112eb8a850052f8f96bf"
+    default: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1694173844~exp=1694174444~hmac=9f6dbea3ec57a24bafc0852688622c6a605c4a70e71d112eb8a850052f8f96bf",
    },
    phone: {
     type:String,
-    default: "+91"
+    default: "+91",
    },
    bio: {
     type:String,
     maxLength: [250, "Bio must not be more than 250 characters"],
-    default:"Bio" 
-   } 
-}, {
-    timestamps: true
-})
+    default:"Bio", 
+   }, 
+}, 
+{
+    timestamps: true,
+}
+);
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+
 
 const app = express();
 
 // Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
+// Route Middleware
+app.use("/api/users", userRoute);
 
 // Routes
 app.get("/", (req, res) =>{
   res.send("Home Page");
-})
+});
 
 const PORT = process.env.PORT || 3993;
 
-// connect to DB and start server
+// Error Middleware
+app.use(errorHandler);
 
+// connect to DB and start server
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
